09b6c88da236b14707f253c7fb25794223c28ec8
ryan <ryan.hahn19@yahoo.com>
2018-05-09 23:37:40 -0500

    Changed Header

--- MovieApplication1/Controllers/CustomersController.cs
+++ MovieApplication1/Controllers/CustomersController.cs
@@ -75,7 +75,7 @@ namespace MovieApplication1.Controllers
 
         // POST: Customers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "CustomerID,FirstName,LastName,Phone,Email")] Customer customer)
